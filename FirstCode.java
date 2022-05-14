9de0dff9496eeafcad3b7b3bcf339d1dea31f5be
weizr <544872527@qq.com>
2022-05-14 17:09:08 +0800

    new commit

---
+++ FirstCode.java
@@ -0,0 +1,8 @@
+public class FirstCode {
+    public static void main(String[] args) {
+        System.out.println("这是git的第一行代码！");
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("这是一个weizr分支");
+    }
+}
